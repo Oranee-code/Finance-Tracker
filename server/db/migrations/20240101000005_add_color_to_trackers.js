6ddbaf9d20dba34bb5882d1c86b2fbc1ab82fde0
Oranee-Kasem-Whai-25 <ornks43@gmail.com>
2025-12-03 10:40:20 +1300

    knex seed

--- server/db/migrations/20240101000005_add_color_to_trackers.js
+++ server/db/migrations/20240101000005_add_color_to_trackers.js
@@ -3,7 +3,7 @@
  */
 export async function up(knex) {
   return knex.schema.alterTable('trackers', (table) => {
-    table.string('color').defaultTo('#0d9488') // Default to luxury-teal
+    table.string('color').defaultTo('#0d9488')
   })
 }
 
@@ -15,4 +15,3 @@ export async function down(knex) {
     table.dropColumn('color')
   })
 }
-

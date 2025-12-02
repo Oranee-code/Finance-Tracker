da3bfe5bb36d59bf946a57e659e0adfd0489dac7
Oranee-Kasem-Whai-25 <ornks43@gmail.com>
2025-12-03 11:15:32 +1300

    table

--- client/components/auth0.ts
+++
@@ -1,7 +0,0 @@
-export const auth0Config = {
-  domain: import.meta.env.VITE_AUTH0_DOMAIN,
-  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  audience: undefined, // leave undefined for basic login
-  redirectUri: window.location.origin, // must match Auth0 Allowed Callback URL
-}
-
